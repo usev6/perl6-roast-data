96608f2b134452a99688f78e7e0e665544e2b776
usev6 <use_v6@aglaz.de>
2014-09-25 07:39:11 +0200

    workaround for moar segfault setting PERL6LIB

--- bin/rakudo.moar-jit.sh
+++ bin/rakudo.moar-jit.sh
@@ -14,7 +14,8 @@ make all
 
 # uninstalled rakudo doesn't know how to find Test.pm
 # ... or any other modules
-export PERL6LIB=`pwd`/lib:.
+#export PERL6LIB=`pwd`/lib:.
+export PERL6LIB=`pwd`/lib:`pwd`
 
 # some tests require a LANG.
 export LANG=en_US.UTF-8
